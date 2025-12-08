7938d3cb0989a228056c75a6978d72dd920e385f
Aman Shekhar <aman.shekhar4@gmail.com>
2025-12-08 10:31:18 +0530

    feat: solve Striver SDE Sheet problem 395 - Flatten Nested List Iterator

---
+++ striver-sde-sheet/395-341-Flatten-Nested-List-Iterator/solution.java
@@ -0,0 +1,7 @@
+// Solution for Flatten Nested List Iterator
+// Problem ID: 341
+// Link: https://leetcode.com/problems/flatten-nested-list-iterator
+
+class Solution {
+    // TODO: Implement solution
+}
---
+++ striver-sde-sheet/395-341-Flatten-Nested-List-Iterator/solution.py
@@ -0,0 +1,8 @@
+# Solution for Flatten Nested List Iterator
+# Problem ID: 341
+# Link: https://leetcode.com/problems/flatten-nested-list-iterator
+
+class Solution:
+    def solve(self):
+        # TODO: Implement solution
+        pass
