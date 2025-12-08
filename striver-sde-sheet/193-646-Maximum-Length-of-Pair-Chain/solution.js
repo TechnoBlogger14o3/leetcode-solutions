ced8adc67ad38f205788903a432d303fcb3b63f6
Aman Shekhar <aman.shekhar4@gmail.com>
2025-12-08 10:17:15 +0530

    feat: solve Striver SDE Sheet problem 193 - Maximum Length of Pair Chain

---
+++ striver-sde-sheet/193-646-Maximum-Length-of-Pair-Chain/solution.js
@@ -0,0 +1,10 @@
+// Solution for Maximum Length of Pair Chain
+// Problem ID: 646
+// Link: https://leetcode.com/problems/maximum-length-of-pair-chain
+
+/**
+ * TODO: Implement solution
+ */
+var solve = function() {
+    // Implementation needed
+};
