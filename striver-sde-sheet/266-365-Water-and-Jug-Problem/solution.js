e4bf8c6417c2cee714eb9eb8480340babe8ab309
Aman Shekhar <aman.shekhar4@gmail.com>
2025-12-08 10:30:44 +0530

    feat: solve Striver SDE Sheet problem 266 - Water and Jug Problem

---
+++ striver-sde-sheet/266-365-Water-and-Jug-Problem/solution.js
@@ -0,0 +1,10 @@
+// Solution for Water and Jug Problem
+// Problem ID: 365
+// Link: https://leetcode.com/problems/water-and-jug-problem
+
+/**
+ * TODO: Implement solution
+ */
+var solve = function() {
+    // Implementation needed
+};
