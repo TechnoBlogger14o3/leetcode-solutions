42575516b84a716b47eab7c144fd837594cafc74
Aman Shekhar <aman.shekhar4@gmail.com>
2025-12-08 10:30:58 +0530

    feat: solve Striver SDE Sheet problem 327 - Multiply Strings

---
+++ striver-sde-sheet/327-43-Multiply-Strings/solution.java
@@ -0,0 +1,7 @@
+// Solution for Multiply Strings
+// Problem ID: 43
+// Link: https://leetcode.com/problems/multiply-strings
+
+class Solution {
+    // TODO: Implement solution
+}
---
+++ striver-sde-sheet/327-43-Multiply-Strings/solution.py
@@ -0,0 +1,8 @@
+# Solution for Multiply Strings
+# Problem ID: 43
+# Link: https://leetcode.com/problems/multiply-strings
+
+class Solution:
+    def solve(self):
+        # TODO: Implement solution
+        pass
