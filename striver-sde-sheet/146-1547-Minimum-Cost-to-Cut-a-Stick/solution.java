a03b3be3bd029f7ddf74c4989b4248f7ecc6c03b
Aman Shekhar <aman.shekhar4@gmail.com>
2025-12-08 10:14:28 +0530

    feat: solve Striver SDE Sheet problem 146 - Minimum Cost to Cut a Stick

---
+++ striver-sde-sheet/146-1547-Minimum-Cost-to-Cut-a-Stick/solution.java
@@ -0,0 +1,7 @@
+// Solution for Minimum Cost to Cut a Stick
+// Problem ID: 1547
+// Link: https://leetcode.com/problems/minimum-cost-to-cut-a-stick
+
+class Solution {
+    // TODO: Implement solution
+}
---
+++ striver-sde-sheet/146-1547-Minimum-Cost-to-Cut-a-Stick/solution.js
@@ -0,0 +1,10 @@
+// Solution for Minimum Cost to Cut a Stick
+// Problem ID: 1547
+// Link: https://leetcode.com/problems/minimum-cost-to-cut-a-stick
+
+/**
+ * TODO: Implement solution
+ */
+var solve = function() {
+    // Implementation needed
+};
