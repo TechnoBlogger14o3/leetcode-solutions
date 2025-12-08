a43d52a8944ee5e45cae82bd7265a77a590be065
Aman Shekhar <aman.shekhar4@gmail.com>
2025-12-08 10:19:44 +0530

    feat: solve Striver SDE Sheet problem 438 - Next Permutation

---
+++ striver-sde-sheet/438-31-Next-Permutation/solution.js
@@ -0,0 +1,10 @@
+// Solution for Next Permutation
+// Problem ID: 31
+// Link: https://leetcode.com/problems/next-permutation
+
+/**
+ * TODO: Implement solution
+ */
+var solve = function() {
+    // Implementation needed
+};
---
+++ striver-sde-sheet/438-31-Next-Permutation/solution.py
@@ -0,0 +1,8 @@
+# Solution for Next Permutation
+# Problem ID: 31
+# Link: https://leetcode.com/problems/next-permutation
+
+class Solution:
+    def solve(self):
+        # TODO: Implement solution
+        pass
