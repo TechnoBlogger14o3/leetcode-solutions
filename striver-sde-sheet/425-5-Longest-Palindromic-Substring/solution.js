9fec576da05713fdcc1d3840acd0235aa5c01cbb
Aman Shekhar <aman.shekhar4@gmail.com>
2025-12-08 10:12:24 +0530

    feat: solve Striver SDE Sheet problem 425 - Longest Palindromic Substring

---
+++ striver-sde-sheet/425-5-Longest-Palindromic-Substring/solution.js
@@ -0,0 +1,10 @@
+// Solution for Longest Palindromic Substring
+// Problem ID: 5
+// Link: https://leetcode.com/problems/longest-palindromic-substring
+
+/**
+ * TODO: Implement solution
+ */
+var solve = function() {
+    // Implementation needed
+};
---
+++ striver-sde-sheet/425-5-Longest-Palindromic-Substring/solution.py
@@ -0,0 +1,8 @@
+# Solution for Longest Palindromic Substring
+# Problem ID: 5
+# Link: https://leetcode.com/problems/longest-palindromic-substring
+
+class Solution:
+    def solve(self):
+        # TODO: Implement solution
+        pass
