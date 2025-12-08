70513619cccc83a3538ce111832268cc52e9bf07
Aman Shekhar <aman.shekhar4@gmail.com>
2025-12-08 10:32:14 +0530

    feat: solve Striver SDE Sheet problem 225 - Number of Provinces

---
+++ striver-sde-sheet/225-547-Number-of-Provinces/solution.java
@@ -0,0 +1,7 @@
+// Solution for Number of Provinces
+// Problem ID: 547
+// Link: https://leetcode.com/problems/number-of-provinces
+
+class Solution {
+    // TODO: Implement solution
+}
---
+++ striver-sde-sheet/225-547-Number-of-Provinces/solution.js
@@ -0,0 +1,10 @@
+// Solution for Number of Provinces
+// Problem ID: 547
+// Link: https://leetcode.com/problems/number-of-provinces
+
+/**
+ * TODO: Implement solution
+ */
+var solve = function() {
+    // Implementation needed
+};
