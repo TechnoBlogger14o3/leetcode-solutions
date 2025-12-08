2acc163a12c6cef51fbd05366b4c3e2619fc978c
Aman Shekhar <aman.shekhar4@gmail.com>
2025-12-08 09:49:57 +0530

    feat: solve Striver SDE Sheet problem 31 - Trapping Rain Water

---
+++ striver-sde-sheet/031-42-Trapping-Rain-Water/solution.js
@@ -0,0 +1,10 @@
+// Solution for Trapping Rain Water
+// Problem ID: 42
+// Link: https://leetcode.com/problems/trapping-rain-water
+
+/**
+ * TODO: Implement solution
+ */
+var solve = function() {
+    // Implementation needed
+};
---
+++ striver-sde-sheet/031-42-Trapping-Rain-Water/solution.py
@@ -0,0 +1,8 @@
+# Solution for Trapping Rain Water
+# Problem ID: 42
+# Link: https://leetcode.com/problems/trapping-rain-water
+
+class Solution:
+    def solve(self):
+        # TODO: Implement solution
+        pass
