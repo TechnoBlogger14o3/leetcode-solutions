0dc14d79d9036b03eaa1c7ece53230fcdae0bc25
Aman Shekhar <aman.shekhar4@gmail.com>
2025-12-08 10:19:30 +0530

    feat: solve Striver SDE Sheet problem 394 - Evaluate Reverse Polish Notation

---
+++ striver-sde-sheet/394-150-Evaluate-Reverse-Polish-Notation/solution.java
@@ -0,0 +1,7 @@
+// Solution for Evaluate Reverse Polish Notation
+// Problem ID: 150
+// Link: https://leetcode.com/problems/evaluate-reverse-polish-notation
+
+class Solution {
+    // TODO: Implement solution
+}
---
+++ striver-sde-sheet/394-150-Evaluate-Reverse-Polish-Notation/solution.js
@@ -0,0 +1,10 @@
+// Solution for Evaluate Reverse Polish Notation
+// Problem ID: 150
+// Link: https://leetcode.com/problems/evaluate-reverse-polish-notation
+
+/**
+ * TODO: Implement solution
+ */
+var solve = function() {
+    // Implementation needed
+};
