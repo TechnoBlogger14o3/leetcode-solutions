d253b04b691e4c73e54d54cb4c7c1192c7b425a8
Aman Shekhar <aman.shekhar4@gmail.com>
2025-12-08 10:11:21 +0530

    feat: solve Striver SDE Sheet problem 374 - Partition Array According to Given Pivot

---
+++ striver-sde-sheet/374-2161-Partition-Array-According-to-Given-Pivot/solution.js
@@ -0,0 +1,10 @@
+// Solution for Partition Array According to Given Pivot
+// Problem ID: 2161
+// Link: https://leetcode.com/problems/partition-array-according-to-given-pivot
+
+/**
+ * TODO: Implement solution
+ */
+var solve = function() {
+    // Implementation needed
+};
