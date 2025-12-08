790292699ce8a49f5ac698800be6000aeb5b4424
Aman Shekhar <aman.shekhar4@gmail.com>
2025-12-08 09:50:01 +0530

    feat: solve Striver SDE Sheet problem 44 - Sudoku Solver

---
+++ striver-sde-sheet/044-37-Sudoku-Solver/solution.java
@@ -0,0 +1,7 @@
+// Solution for Sudoku Solver
+// Problem ID: 37
+// Link: https://leetcode.com/problems/sudoku-solver
+
+class Solution {
+    // TODO: Implement solution
+}
---
+++ striver-sde-sheet/044-37-Sudoku-Solver/solution.py
@@ -0,0 +1,8 @@
+# Solution for Sudoku Solver
+# Problem ID: 37
+# Link: https://leetcode.com/problems/sudoku-solver
+
+class Solution:
+    def solve(self):
+        # TODO: Implement solution
+        pass
