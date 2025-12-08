99d13ededcbb042101a38670f08ec902715bf97a
Aman Shekhar <aman.shekhar4@gmail.com>
2025-12-08 10:19:52 +0530

    feat: solve Striver SDE Sheet problem 472 - Implement Trie (Prefix Tree)

---
+++ striver-sde-sheet/472-208-Implement-Trie-Prefix-Tree/solution.java
@@ -0,0 +1,7 @@
+// Solution for Implement Trie (Prefix Tree)
+// Problem ID: 208
+// Link: https://leetcode.com/problems/implement-trie-prefix-tree
+
+class Solution {
+    // TODO: Implement solution
+}
