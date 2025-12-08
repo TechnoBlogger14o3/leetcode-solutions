2883acab26dea9e6f6d610210e4eb7261887b5a2
Aman Shekhar <aman.shekhar4@gmail.com>
2025-12-08 10:09:30 +0530

    feat: solve Striver SDE Sheet problem 253 - Number of Islands

---
+++ striver-sde-sheet/253-200-Number-of-Islands/solution.py
@@ -0,0 +1,8 @@
+# Solution for Number of Islands
+# Problem ID: 200
+# Link: https://leetcode.com/problems/number-of-islands
+
+class Solution:
+    def solve(self):
+        # TODO: Implement solution
+        pass
