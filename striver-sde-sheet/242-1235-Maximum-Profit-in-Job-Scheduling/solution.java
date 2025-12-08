5d3296921fb2e2f61254c5af372f32e496745061
Aman Shekhar <aman.shekhar4@gmail.com>
2025-12-08 10:19:10 +0530

    feat: solve Striver SDE Sheet problem 242 - Maximum Profit in Job Scheduling

---
+++ striver-sde-sheet/242-1235-Maximum-Profit-in-Job-Scheduling/solution.java
@@ -0,0 +1,7 @@
+// Solution for Maximum Profit in Job Scheduling
+// Problem ID: 1235
+// Link: https://leetcode.com/problems/maximum-profit-in-job-scheduling
+
+class Solution {
+    // TODO: Implement solution
+}
---
+++ striver-sde-sheet/242-1235-Maximum-Profit-in-Job-Scheduling/solution.py
@@ -0,0 +1,8 @@
+# Solution for Maximum Profit in Job Scheduling
+# Problem ID: 1235
+# Link: https://leetcode.com/problems/maximum-profit-in-job-scheduling
+
+class Solution:
+    def solve(self):
+        # TODO: Implement solution
+        pass
