f04fd46d2008ccd0252003336ba51acd383c4856
Aman Shekhar <aman.shekhar4@gmail.com>
2025-12-08 09:50:26 +0530

    feat: solve Striver SDE Sheet problem 409 - LRU Cache

---
+++ striver-sde-sheet/409-146-LRU-Cache/solution.java
@@ -0,0 +1,7 @@
+// Solution for LRU Cache
+// Problem ID: 146
+// Link: https://leetcode.com/problems/lru-cache
+
+class Solution {
+    // TODO: Implement solution
+}
---
+++ striver-sde-sheet/409-146-LRU-Cache/solution.py
@@ -0,0 +1,8 @@
+# Solution for LRU Cache
+# Problem ID: 146
+# Link: https://leetcode.com/problems/lru-cache
+
+class Solution:
+    def solve(self):
+        # TODO: Implement solution
+        pass
