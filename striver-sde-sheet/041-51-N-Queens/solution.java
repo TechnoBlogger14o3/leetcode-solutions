ea8e5f9eb04db5edcbe9e83eb45e8e1d479f8315
Aman Shekhar <aman.shekhar4@gmail.com>
2025-12-08 10:14:18 +0530

    feat: solve Striver SDE Sheet problem 41 - N-Queens

---
+++ striver-sde-sheet/041-51-N-Queens/solution.java
@@ -0,0 +1,7 @@
+// Solution for N-Queens
+// Problem ID: 51
+// Link: https://leetcode.com/problems/n-queens
+
+class Solution {
+    // TODO: Implement solution
+}
---
+++ striver-sde-sheet/041-51-N-Queens/solution.js
@@ -0,0 +1,10 @@
+// Solution for N-Queens
+// Problem ID: 51
+// Link: https://leetcode.com/problems/n-queens
+
+/**
+ * TODO: Implement solution
+ */
+var solve = function() {
+    // Implementation needed
+};
