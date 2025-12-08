df0efec05ef3ef0575041a1e4bf5a9066f527478
Aman Shekhar <aman.shekhar4@gmail.com>
2025-12-08 09:49:17 +0530

    feat: solve Striver SDE Sheet problem 305 - Add Two Numbers

--- striver-sde-sheet/305-2-Add-Two-Numbers/solution.java
+++ striver-sde-sheet/305-2-Add-Two-Numbers/solution.java
@@ -1,7 +1,32 @@
-// Solution for Add Two Numbers
-// Problem ID: 2
-// Link: https://leetcode.com/problems/add-two-numbers
-
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
 class Solution {
-    // TODO: Implement solution
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(0);
+        ListNode current = dummy;
+        int carry = 0;
+        
+        while (l1 != null || l2 != null || carry != 0) {
+            int val1 = (l1 != null) ? l1.val : 0;
+            int val2 = (l2 != null) ? l2.val : 0;
+            
+            int total = val1 + val2 + carry;
+            carry = total / 10;
+            current.next = new ListNode(total % 10);
+            current = current.next;
+            
+            l1 = (l1 != null) ? l1.next : null;
+            l2 = (l2 != null) ? l2.next : null;
+        }
+        
+        return dummy.next;
+    }
 }
--- striver-sde-sheet/305-2-Add-Two-Numbers/solution.js
+++ striver-sde-sheet/305-2-Add-Two-Numbers/solution.js
@@ -1,10 +1,32 @@
-// Solution for Add Two Numbers
-// Problem ID: 2
-// Link: https://leetcode.com/problems/add-two-numbers
-
 /**
- * TODO: Implement solution
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
  */
-var solve = function() {
-    // Implementation needed
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var addTwoNumbers = function(l1, l2) {
+    const dummy = new ListNode(0);
+    let current = dummy;
+    let carry = 0;
+    
+    while (l1 || l2 || carry) {
+        const val1 = l1 ? l1.val : 0;
+        const val2 = l2 ? l2.val : 0;
+        
+        const total = val1 + val2 + carry;
+        carry = Math.floor(total / 10);
+        current.next = new ListNode(total % 10);
+        current = current.next;
+        
+        l1 = l1 ? l1.next : null;
+        l2 = l2 ? l2.next : null;
+    }
+    
+    return dummy.next;
 };
