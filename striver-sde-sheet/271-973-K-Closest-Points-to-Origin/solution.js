785de9b0616622fc7b367576ac7e684fc5b3ad26
Aman Shekhar <aman.shekhar4@gmail.com>
2025-12-08 10:30:50 +0530

    feat: solve Striver SDE Sheet problem 271 - K Closest Points to Origin

---
+++ striver-sde-sheet/271-973-K-Closest-Points-to-Origin/solution.js
@@ -0,0 +1,10 @@
+// Solution for K Closest Points to Origin
+// Problem ID: 973
+// Link: https://leetcode.com/problems/k-closest-points-to-origin
+
+/**
+ * TODO: Implement solution
+ */
+var solve = function() {
+    // Implementation needed
+};
