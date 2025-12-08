18301999fc6fe11e8a4df5af411d7e2f9bc6fa7a
Aman Shekhar <aman.shekhar4@gmail.com>
2025-12-08 10:07:15 +0530

    feat: solve Striver SDE Sheet problem 98 - Same Tree

---
+++ striver-sde-sheet/098-100-Same-Tree/solution.js
@@ -0,0 +1,10 @@
+// Solution for Same Tree
+// Problem ID: 100
+// Link: https://leetcode.com/problems/same-tree
+
+/**
+ * TODO: Implement solution
+ */
+var solve = function() {
+    // Implementation needed
+};
