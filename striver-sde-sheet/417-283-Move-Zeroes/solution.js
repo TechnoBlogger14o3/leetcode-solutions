c5e9f085d3e8000b3c94cfb8e371fb9cc2fb98de
Aman Shekhar <aman.shekhar4@gmail.com>
2025-12-08 10:15:22 +0530

    feat: solve Striver SDE Sheet problem 417 - Move Zeroes

---
+++ striver-sde-sheet/417-283-Move-Zeroes/solution.js
@@ -0,0 +1,10 @@
+// Solution for Move Zeroes
+// Problem ID: 283
+// Link: https://leetcode.com/problems/move-zeroes
+
+/**
+ * TODO: Implement solution
+ */
+var solve = function() {
+    // Implementation needed
+};
---
+++ striver-sde-sheet/417-283-Move-Zeroes/solution.py
@@ -0,0 +1,8 @@
+# Solution for Move Zeroes
+# Problem ID: 283
+# Link: https://leetcode.com/problems/move-zeroes
+
+class Solution:
+    def solve(self):
+        # TODO: Implement solution
+        pass
