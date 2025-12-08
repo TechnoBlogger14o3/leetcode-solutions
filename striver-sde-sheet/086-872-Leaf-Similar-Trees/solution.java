4da0eee6999ed4cd42a0d3427f46ae7f3a70a180
Aman Shekhar <aman.shekhar4@gmail.com>
2025-12-08 10:16:16 +0530

    feat: solve Striver SDE Sheet problem 86 - Leaf-Similar Trees

---
+++ striver-sde-sheet/086-872-Leaf-Similar-Trees/solution.java
@@ -0,0 +1,7 @@
+// Solution for Leaf-Similar Trees
+// Problem ID: 872
+// Link: https://leetcode.com/problems/leaf-similar-trees
+
+class Solution {
+    // TODO: Implement solution
+}
---
+++ striver-sde-sheet/086-872-Leaf-Similar-Trees/solution.js
@@ -0,0 +1,10 @@
+// Solution for Leaf-Similar Trees
+// Problem ID: 872
+// Link: https://leetcode.com/problems/leaf-similar-trees
+
+/**
+ * TODO: Implement solution
+ */
+var solve = function() {
+    // Implementation needed
+};
